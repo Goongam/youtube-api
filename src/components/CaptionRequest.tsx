78263da31f9805a12816aa759bca35c160249b30
Goongam <dlwjddn341@naver.com>
2023-12-02 23:52:59 +0900

    feat: 자막요청 에러처리

--- src/components/CaptionRequest.tsx
+++ src/components/CaptionRequest.tsx
@@ -1,17 +1,37 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export default function CaptionRequest() {
   const searchParams = useSearchParams();
   const captionId = searchParams.get("caption");
+
+  const [caption, setCaption] = useState("");
+  const [error, setError] = useState(false);
+
   const request = () => {
     fetch(`/api/youtube/download/${captionId}`, {
       // credentials: "include",
     })
-      .then((res) => res.json())
-      .then((json) => console.log(json));
+      .then((res) => {
+        if (res.ok) return res.json();
+        else setError(true);
+      })
+      .then((caption) => {
+        setCaption(caption);
+      })
+      .catch(() => {
+        setError(true);
+      });
   };
 
-  return <button onClick={request}>자막요청</button>;
+  if (error) return <div>에러...</div>;
+
+  return (
+    <>
+      <button onClick={request}>자막요청</button>
+      <div>{caption}</div>
+    </>
+  );
 }

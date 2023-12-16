91170b15b299ae2944fae0b0fc95f2984d24f15c
Goongam <dlwjddn341@naver.com>
2023-12-16 22:28:55 +0900

    style: 번역 카드 최소 높이 지정

--- src/components/CaptionListView.tsx
+++ src/components/CaptionListView.tsx
@@ -6,13 +6,20 @@ interface Props {
 }
 export default function CaptionListView({ captions, modify = false }: Props) {
   return (
-    <div className="flex flex-col gap-1 flex-1 border border-black">
+    <div className="flex flex-col gap-1 flex-1">
       {captions.map(({ endTime, startTime, text }, i) => (
-        <div key={i} className="flex flex-col shadow-md rounded-sm p-2 m-2">
+        <div
+          key={i}
+          className="flex flex-col shadow-md rounded-sm p-2 m-2 min-h-[100px]"
+        >
           <div className="text-black/50 text-sm">
             {startTime} - {endTime}
           </div>
-          {modify ? <textarea>{text}</textarea> : <div>{text}</div>}
+          {modify ? (
+            <textarea className="h-full">{text}</textarea>
+          ) : (
+            <div>{text}</div>
+          )}
         </div>
       ))}
     </div>
--- src/components/CaptionRequest.tsx
+++ src/components/CaptionRequest.tsx
@@ -2,7 +2,7 @@
 //https://www.youtube.com/watch?v=j40_yYh9OvI
 import { TLANGS } from "@/constants/tlang";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import TranslateRequest from "./TranslateRequest";
 import { Caption, generateCaption } from "@/util/captions";
 import CaptionListView from "./CaptionListView";
@@ -15,9 +15,7 @@ export default function CaptionRequest() {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  console.log(caption);
-
-  const request = () => {
+  const request = useCallback(() => {
     setLoading(true);
     fetch(`/api/youtube/download/${captionId}`, {
       // credentials: "include",
@@ -35,7 +33,11 @@ export default function CaptionRequest() {
       .finally(() => {
         setLoading(false);
       });
-  };
+  }, [captionId]);
+
+  useEffect(() => {
+    request();
+  }, [request]);
 
   if (loading) return <>loading...</>;
   if (error)
@@ -48,7 +50,7 @@ export default function CaptionRequest() {
 
   return (
     <>
-      <button onClick={request}>자막요청</button>
+      {/* <button onClick={request}>자막요청</button> */}
       <div className="flex p-2 gap-2">
         <CaptionListView captions={caption} />
         <TranslateRequest />
